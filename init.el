e1c2dcc6d4e3d3edbf006ef4e4c836a25d7acec8
kosuzu <htetmyatminnn@gmail.com>
2024-08-11 03:01:38 +0800

    Update init.el
    Replaced Ivy and Counsel with Vertico and Consult. Added modeline configuration with doom-modeline. Moved base UI configurations, line number and columns configuration and custom font to early-init.el

--- init.el
+++ init.el
@@ -1,42 +1,34 @@
 ;; init.el Configurations
 ;; From kosuzu
 
-(setq inhibit-startup-message 1)	; Disable the startup message.
+(load (concat user-emacs-directory "early-init.el")) ; Load up early-init.el configs
 
-(add-hook 'emacs-startup-hook 'toggle-frame-maximized) ; Fullscreen on startup 
+(load-theme 'ef-dream t)		; Set custom theme
 
-(scroll-bar-mode -1) 			; Disable scrollbar
-(tooltip-mode -1)			; Disable tooltips
-(menu-bar-mode -1)			; Disable menubar
-(tool-bar-mode -1)			; Disable toolbar
-(set-fringe-mode 15) 			; More spaceous!
+;; Enable rainbow delimiters for improved syntax reading in prog-mode
+(use-package rainbow-delimiters
+    :hook (prog-mode . rainbow-delimiters-mode))
 
-(setq visible-bell t)			; Enable visual bell
-
-(load-theme 'ef-dream  t)		; Set custom theme
-
-(set-face-attribute 'default nil :font "CascadiaMono" :height 100) ; Set custom font
-
-;; Enable line numbers and columns
-(setq column-number-mode t)	   
-(display-line-numbers-mode t)	   ; Set to absolute by default
-(global-display-line-numbers-mode) ; Enabled so line numbers are persistent
-
-;; Disable line numbers on selected modes
-(dolist (mode '(org-mode-hook
-		term-mode-hook
-		vterm-mode-hook
-		shell-mode-hook
-		treemacs-mode-hook
-		eshell-mode-hook))
-  (add-hook mode (lambda() (display-line-numbers-mode 0))))
+;; == Modeline ==
+;; Doom modeline requires nerd-icons package to be installed 
+(use-package doom-modeline
+    :config
+  (setq doom-modeline-height 15)	; Adjust modeline height 
+  (setq doom-modeline-project-detection 'project) ; Detect project root
+  (setq doom-modeline-icon t)			  ; Enable modeline icons (on by default)
+  (setq doom-modeline-position-column-line-format '("%l|%c")) ; line/column numbers format
+  (setq doom-modeline-total-line-number t)		      ; Display total line numbers
+  (setq doom-mdeline-minor-modes t)			      ; Display minor modes
+  (setq doom-modeline-gnus t)				      ;Display GNUs notifs
+  (setq doom-modeline-gnus-timer 2)
+  :init
+  (doom-modeline-mode 1))
 
 ;; Org mode Config
 (require 'org)		; Make Org mode work with files ending in .org
 
-;; Package source initialisation
+;; ==  Package source initialisation & manager  ==
 (require 'package)
-
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
 			 ("org" . "https://orgmode.org/elpa/")
 			 ("elpa" . "https://elpa.gnu.org/packages/")))
@@ -50,9 +42,9 @@
   (package-install 'use-package))
 
 (require 'use-package)
-(setq use-package-always-ensure t)
+(setq use-package-always-ensure t) ; Removes the need to do ':ensure t', mostly. 
 
-;; Set up automatic package updater
+;; Set up automatic package updater 
 (use-package auto-package-update
     :custom
   (auto-package-update-interval 2)	; Update every 2 days
@@ -61,45 +53,39 @@
   :config
   (auto-package-update-maybe))
 
-;; Enable rainbow delimiters for improved syntax reading in prog-mode
-(use-package rainbow-delimiters
-    :hook (prog-mode . rainbow-delimiters-mode))
+(require 'project) ; Use built-in package.el 
 
-;; Set up built-in project package
-(require 'project)
-
-;; ==
-
-;; == KEYBINDS & KEYMAPS ==
-
-;; Set up Ivy keybinds
-(use-package ivy
-    :diminish
-  :bind (("C-s" . swiper)
-	 :map ivy-minibuffer-map
-	 ("TAB" . ivy-alt-done)
-	 ("C-l" . ivy-alt-done)
-	 ("C-j" . ivy-next-line) 
-	 ("C-k" . ivy-previous-line) 
-	 :map ivy-switch-buffer-map
-	 ("C-k" . ivy-previous-line)
-	 ("C-l" . ivy-done)
-	 ("C-d" . ivy-switch-buffer-kill)
-	 :map ivy-reverse-i-search-map
-	 ("C-k" . ivy-previous-line)
-	 ("C-d" . ivy-reverse-i-search-kill))
+;; == Auto-complete, minibuffer, and  Keybindings ==
+;; Set up Vertico (completion UI)
+(use-package vertico
+  :demand t
+  :bind(:map vertico-map
+	     ("C-j" . vertico-next)
+	     ("C-k" . vertico-previous)
+	     ("C-f" . vertico-exit)
+	     :map minibuffer-local-map
+	     ("C-j" . vertico-next)
+	     ("C-k" . vertico-previous)
+	     ("C-b" . vertico-)
+	     ("M-h" . backward-killword))
+  :custom
+  (vertico-resize t)
+  (vertico-cycle nil)		  ; Disabled cycling for next/previous
+  :init
+  (vertico-grid-mode)	 ; Change minibuffer appearance to grid format
+  (vertico-indexed-mode) ; Allows for index selection with prefix arguments
   :config
-  (ivy-mode t))
+  (vertico-mode))
 
-;; Set up Counsel keybinds
-(use-package counsel
-    :bind (("M-x" . counsel-M-x)	; Replaces the default "M-x"
-	   ("C-x b" . counsel-ibuffer)
-	   ("C-x C-f" . counsel-find-file)
+;; Set up Consult
+(use-package consult
+    :demand t
+    :bind (("C-s" . consult-line)
+	   ("C-M-l" . consult-imenu)
+	   ("C-M-j" . persp-switch-to-buffer*)
+	   ("C-x b" . consult-buffer)
 	   :map minibuffer-local-map
-	   ("C-r" . 'counsel-minibuffer-history))
-    :config
-    (setq ivy-initial-inputs-alist nil))
+	   ("C-r" . consult-history)))
 
 ;; Set up Hydra keybinds
 (use-package hydra)
@@ -131,14 +117,14 @@
 
 ;; ==
 
-;;  Magit Configurations
+;; Magit Configurations
 
 (use-package magit
   :custom
   (magit-display-buffer-function #'magit-display-buffer-window-except-diff-v1))
 
 
-;; == IDE SETUP
+;; == IDE Setup ==
 
 ;; Set up lsp-mode
 (use-package lsp-mode
@@ -157,46 +143,43 @@
 
 ;; Set up user interface
 (use-package lsp-ui
-    :hook (lsp-mode . lsp-ui-mode)
-    :commands (lsp-ui-mode)
-    :config
-    (setq lsp-ui-doc-enable nil)
-    (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
-    (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references))
-
-;; Interactive ivy interface for lsp-mode
-(use-package lsp-ivy
-    :commands lsp-ivy-workspace-symbol)
+  :hook (lsp-mode . lsp-ui-mode)
+  :commands (lsp-ui-mode)
+  :config
+  (setq lsp-ui-doc-enable nil)
+  (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+  (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references))
 
 ;; Set up treemacs
 (use-package treemacs) 
 
 ;; Integrate lsp-mode and treemacs
 (use-package lsp-treemacs
-    :after lsp
+  :after lsp
     :commands lsp-treemacs-errors-list)
 (lsp-treemacs-sync-mode t); Enables syncing between lsp workspace folders and treemac projects
 
 ;; Set up company-mode
 (use-package company
-    :init (add-hook 'after-init-hook 'global-company-mode)
-    :config
-    (setq company-show-numbers t
-	  company-minimum-prefix-length 1
-	  company-idle-delay 0.5
-	  company-backends
-	  '((company-files		; Files & directory
-	     company-keywords
-	     company-capf  ; Backend for completion-at-point-functions
-	     company-yasnippet)
-	    (company-abbrev company-dabbrev))))
+  :init (add-hook 'after-init-hook 'global-company-mode)
+  :config
+  (setq company-show-numbers t
+	company-minimum-prefix-length 1
+	company-idle-delay 0.5
+	company-backends
+	'((company-files		; Files & directory
+	   company-keywords
+	   company-capf  ; Backend for completion-at-point-functions
+	   company-yasnippet)
+	  (company-abbrev company-dabbrev))))
 
 (use-package flycheck
-    :init (global-flycheck-mode)
-    :config
-    (setq flycheck-display-errors-function
-	  #'flycheck-display-error-messages-unless-error-list)
-    (setq flycheck-indication-mode nil))
+  :init (global-flycheck-mode)
+  :config
+  (setq flycheck-display-errors-function
+	#'flycheck-display-error-messages-unless-error-list)
+  (setq flycheck-indication-mode nil))
+
 
 ;; Set up the ccls language server for C/C++
 (use-package ccls
@@ -215,8 +198,10 @@
 		 (concat "clang -Wall -std=c11 -lm -o")))) ; -Wall for warnings. -o is to tell where to store the compiler output. Using C11 standard. 
 
 ;; ==
-;; == TERMINAL MODES
 
+;; == Terminal == 
+
+;; Set-up vterm. libvterm library required.
 (use-package vterm
     :commands vterm
     :config
@@ -224,15 +209,6 @@
     (setq vterm-max-scrollback 10000))
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(magit auto-package-update auto-package vterm ef-themes ccls flycheck company lsp-treemacs treemacs lsp-ivy lsp-ui lsp-mode which-key hydra counsel ivy rainbow-delimiters)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+   '(doom-modeline which-key vterm vertico rainbow-delimiters orderless magit lsp-ui lsp-treemacs lsp-ivy flycheck ef-themes counsel consult company ccls auto-package-update)))
+(custom-set-faces)
