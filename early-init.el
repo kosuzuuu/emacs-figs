d9af619034f3fb32489fb24c93dedea589454a45
kosuzu <htetmyatminnn@gmail.com>
2024-08-15 20:35:30 +0800

    Update early-init.el

--- early-init.el
+++ early-init.el
@@ -1,10 +1,15 @@
-;; early-init.el Configurations
+;;; early-init.el Configurations
 ;;  From kosuzu
 
 ;; == Garbage Collection ==
-;; Increasing the GC threshold for quicker startups
 ;; By default its set to 800 KB.
-(setq gc-cons-threshold 100000000)	; Here its set to 100MB
+;; Increasing the GC threshold for quicker startups
+(setq gc-cons-threshold most-positive-fixnum)
+(add-hook 'emacs-startup-hook
+	  (lambda ()
+	    ;; Set the GC threshold to 100MB after startup
+	    (setq gc-cons-threshold 100000000)))
+
 (setq garbage-collection-messages t)	; Enable garabge collection message
 
 ;; == UI Configs ==
@@ -17,7 +22,7 @@
 (setq visible-bell t)			; Enable visual bell
 
 ;; == Line numbers & columns ==
-(setq column-number-mode t)	   
+(setq column-number-mode t)
 (display-line-numbers-mode t)	   ; Set to absolute by default
 (global-display-line-numbers-mode) ; Enabled so line numbers are persistent
 
@@ -30,7 +35,11 @@
 		eshell-mode-hook))
   (add-hook mode (lambda() (display-line-numbers-mode 0))))
 
-
 (add-hook 'emacs-startup-hook 'toggle-frame-maximized) ; Fullscreen on startup
+(set-face-attribute 'default nil :font "CascadiaMono" :height 115) ; Set custom font and size
 
-(set-face-attribute 'default nil :font "CascadiaMono" :height 100) ; Set custom font
+(setq history-delete-duplicates t)	; Delete previous identical elements from history list
+
+;; == Scratch buffer ==
+(setq initial-scratch-message nil)	; Disable the message in  scratch buffer
+(setq initial-major-mode 'org-mode)	; Start the scratch buffer in org mode
