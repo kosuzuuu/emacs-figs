b62021643cbea89b7c6a788038aad91ef632ca6c
kosuzu <htetmyatminnn@gmail.com>
2024-08-17 13:31:13 +0800

    Update init.el

--- init.el
+++ init.el
@@ -1,13 +1,13 @@
 ;;; init.el Configurations
-;; From kosuzu
+;;; From kosuzu
 
-(load (concat user-emacs-directory "early-init.el")) ; Load up early-init.el configs
-
-(load-theme 'ef-dream t)		; Set custom theme
-
-;; Enable rainbow delimiters for improved syntax reading in prog-mode
-(use-package rainbow-delimiters
-    :hook (prog-mode . rainbow-delimiters-mode))
+;; == Load files ==
+;; == early-init.el
+(setq early-init-file (expand-file-name "early-init.el" user-emacs-directory))
+(load early-init-file) ; Load early-init.el
+;; == custom.el
+(defconst custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file) ; Load custom.el
 
 ;; == Modeline ==
 ;; Doom modeline requires nerd-icons package to be installed
@@ -18,14 +18,13 @@
   (setq doom-modeline-icon t)			  ; Enable modeline icons (on by default)
   (setq doom-modeline-position-column-line-format '("%l|%c")) ; line/column numbers format
   (setq doom-modeline-total-line-number t)		      ; Display total line numbers
-  (setq doom-modeline-gnus t)				      ;Display GNUs notifs
+  (setq doom-modeline-gnus t)				      ; Display GNUs notifs
   (setq doom-modeline-gnus-timer 2)
   :init
-  (doom-modeline-mode 1))
+  (doom-modeline-mode 1)) ; Initialise doom modeline
 
 ;; == Org mode ==
-(require 'org)		; Make Org mode work with files ending in .org
-
+(require 'org) ; Make Org mode work with files ending in .org
 
 ;; ==  Package source initialisation & manager  ==
 (require 'package)
@@ -44,7 +43,7 @@
 (require 'use-package)
 (setq use-package-always-ensure t) ; Removes the need to do ':ensure t', mostly.
 
-;; Set up automatic package updater
+;; == automatic package updater
 (use-package auto-package-update
   :custom
   (auto-package-update-interval 2)	; Update every 2 days
@@ -69,14 +68,9 @@
 (use-package treemacs-icons-dired
   :hook (dired-mode . treemacs-icons-dired-mode))
 
-;; Set up dired quick sort, binded to 'S' key,
-;(require 'dired-quick-sort) ; Cant set it up for now, might need to redo later.
-; (dired-quick-sort-setup)
-
-
 ;; == Auto-complete & minibuffer configs ==
-;; Set up Vertico (completion UI)
-(use-package vertico 
+;; == Vertico (completion UI)
+(use-package vertico
   :demand t
   :bind(:map vertico-map
 	     ("C-j" . vertico-next)
@@ -96,7 +90,7 @@
   :config
   (vertico-mode))
 
-;; Set up Consult
+;; == Consult
 (use-package consult
     :demand t
     :bind (("C-s" . consult-line)
@@ -107,8 +101,8 @@
 	   ("C-r" . consult-history)))
 
 
-;;  == Global & Hydra keybindings == 
-;; Set up Hydra keybinds
+;; == Global & Hydra keybindings ==
+;; == Hydra
 (use-package hydra)
 
 ;; Text scaling keybind
@@ -127,25 +121,65 @@
   ("r" (setq display-line-numbers 'relative) "Set to relative" :exit t))
 (global-set-key (kbd "C-c l") 'hydra-line-number/body) ; Set the keybind
 
-(global-set-key (kbd "C-c v") 'vterm)	; vterm keybind
+;; == Evil Mode (vim bindings)
+(use-package general
+  :config
+  (general-evil-setup t)
 
-;; Set up which-key for keybind references
+  (general-create-definer rune/leader-keys
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "C-SPC"))
+
+(defun rune/evil-hook ()
+  (dolist (mode '(custom-mode
+                  eshell-mode
+                  git-rebase-mode
+                  erc-mode
+                  circe-server-mode
+                  circe-chat-mode
+                  circe-query-mode
+                  sauron-mode
+                  term-mode))
+   (add-to-list 'evil-emacs-state-modes mode)))
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :hook (evil-mode . rune/evil-hook)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+;; == which-key (Keybind reference)
 (use-package which-key
   :init (which-key-mode)
   :diminish which-key-mode
   :config
   (setq which-key-idle-delay 0.1))
 
-
 ;; == Magit Configurations ==
-
 (use-package magit
   :custom
   (magit-display-buffer-function #'magit-display-buffer-window-except-diff-v1))
 
-
-;; == IDE == 
-;; Set up lsp-mode
+;; == LSP ==
 (use-package lsp-mode
     :hook ((c-mode . lsp)
 	   (c++-mode . lsp)
@@ -158,36 +192,34 @@
     (setq lsp-enable-on-type-formatting nil) ; Re-enable if this is useful for you
     :custom
     (setq lsp-prefer-flymake nil) ; Disabled  so that lsp-mode uses flycheck
-    (setq lsp-enable-global-mode nil)	; Disable lsp-mode by default unless explicitly toggled on 
+    (setq lsp-enable-global-mode nil)	; Disable lsp-mode by default unless explicitly toggled on
     (require 'ccls)
     (setq ccls-executable "/usr/bin/ccls"))
 
-;; Set up flycheck
+;; == LSP UI ==
+(use-package lsp-ui
+  :hook (lsp-mode . lsp-ui-mode)	; Hooked to lsp-mode
+  :commands (lsp-ui-mode)
+  :config
+  (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+  (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references))
+
+;; == Flycheck ==
 (use-package flycheck
   :init (global-flycheck-mode)
   :config
   (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
   (setq flycheck-indication-mode nil))
 
-;; Set up user interface
-(use-package lsp-ui
-  :hook (lsp-mode . lsp-ui-mode)	; Hooked to lsp-mode
-  :commands (lsp-ui-mode)
-  :config
-  (setq lsp-ui-doc-enable nil)
-  (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
-  (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references))
-
-;; Set up treemacs
+;; == Treemacs ==
 (use-package treemacs)
-  
 ;; Integrate lsp-mode and treemacs
 (use-package lsp-treemacs
   :after lsp
     :commands lsp-treemacs-errors-list)
 (lsp-treemacs-sync-mode t); Enables syncing between lsp workspace folders and treemac projects
 
-;; Set up company-mode
+;; == Company Mode ==
 (use-package company
   :init (add-hook 'after-init-hook 'global-company-mode)
   :config
@@ -201,52 +233,56 @@
 	   company-yasnippet)
 	  (company-abbrev company-dabbrev))))
 
-;; Set up the ccls language server for C/C++
+;; == Lang ==
+;; == C and C++
 (use-package ccls
     :hook ((c-mode c++-mode) .
 	   (lambda () (require 'ccls) (lsp))))
-
-;; c-mode Configs
-;; Change default commenting style (Use // instead of /**/)
+;; Change C default commenting style (Use // instead of /**/)
 (add-hook 'c-mode-hook (lambda () (setq comment-start "//"
                                         comment-end   "")))
+
 ;; Configure electric pair mode.
 (electric-pair-mode t)
-(setq electric-pair-preserve-balance -1)
+(setq electric-pair-preserve-balance nil)
 
 ;; Change default compiler command
 (require 'compile)
 (add-hook 'c-mode-hook			; Compiler command with c-mode
 	  (lambda ()
-	   (setq compile-command  
-		 (concat "clang -Wall -std=c17 -lm -o")))) ; -Wall for warnings. -o is to tell where to store the compiler output. Using C17 standard. 
+	   (setq compile-command
+		 (concat "clang -Wall -std=c17 -lm -o")))) ; -Wall for warnings. -o is to tell where to store the compiler output. Using C17 standard.
 (add-hook 'c++-mode-hook				   ; Compiler command with c++-mode
 	  (lambda ()
 	    (setq compile-command
-		  (concat "clang++ -Wall -std=c++17 -lm -o")))) ; -Wall for warnings. -o is to tell where to store the compiler output. Uing C++17 standard. 
+		  (concat "clang++ -Wall -std=c++17 -lm -o")))) ; -Wall for warnings. -o is to tell where to store the compiler output. Uing C++17 standard.
 
-;; == Terminal == 
-;; Set-up vterm. libvterm library required.
+;; == Terminal ==
+;; == Vterm
 (use-package vterm
     :commands vterm
     :config
-    (setq term-prompt-regexp "*[*#$%>\n]*[#$%>] *") 
+    (setq term-prompt-regexp "*[*#$%>\n]*[#$%>] *")
     (setq vterm-max-scrollback 10000))
 
+;; === Misc ===
+;; == treemacs icons for dired
+(use-package treemacs-icons-dired
+  :hook (dired-mode . treemacs-icons-dired-mode))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(auth-source-save-behavior nil)
- '(package-selected-packages
-   '(treemacs-icons-dired doom-modeline which-key vterm vertico rainbow-delimiters orderless magit lsp-ui lsp-treemacs lsp-ivy flycheck ef-themes counsel consult company ccls auto-package-update)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; == Compiler commands
+; Compiler command with c-mode
+(require 'compile)
+(add-hook 'c-mode-hook
+	  (lambda ()
+	   (setq compile-command
+		 (concat "clang -Wall -std=c17 -lm -o")))) ; -Wall for warnings. -o is to tell where to store the compiler output. Using C17 standard.
+; Compiler command with c++-mode
+(add-hook 'c++-mode-hook
+	  (lambda ()
+	    (setq compile-command
+		  (concat "clang++ -Wall -std=c++17 -lm -o")))) ; -Wall for warnings. -o is to tell where to store the compiler output. Uing C++17 standard.
+
 (provide 'early-init)
 (provide 'init)
+;;; init.el ends here
